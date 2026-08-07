6ec9f605fc0b777a277886d7175ec9c748e870b9
amanvishwakarma96 <80578610+amanvishwakarma96@users.noreply.github.com>
2026-08-07 10:36:43 +0530

    fix: use lint-safe challenge backup constructor

--- lib/features/challenges/services/challenge_aware_backup_service.dart
+++ lib/features/challenges/services/challenge_aware_backup_service.dart
@@ -1,23 +1,15 @@
 import 'dart:typed_data';
 
-import 'package:isar_community/isar.dart';
-import 'package:smart_expense_manager/core/security/secure_cipher_service.dart';
 import 'package:smart_expense_manager/features/challenges/data/repositories/weekly_challenge_repository.dart';
-import 'package:smart_expense_manager/features/settings/services/bill_reminder_service.dart';
 import 'package:smart_expense_manager/features/settings/services/local_backup_service.dart';
 
 class ChallengeAwareBackupService extends LocalBackupService {
-  ChallengeAwareBackupService({
-    required Isar isar,
-    required SecureCipherService cipher,
-    required WeeklyChallengeRepository challenges,
-    BillReminderService? reminderService,
-  }) : _challenges = challenges,
-       super(
-         isar: isar,
-         cipher: cipher,
-         reminderService: reminderService,
-       );
+  ChallengeAwareBackupService(
+    this._challenges, {
+    required super.isar,
+    required super.cipher,
+    super.reminderService,
+  });
 
   final WeeklyChallengeRepository _challenges;
 
